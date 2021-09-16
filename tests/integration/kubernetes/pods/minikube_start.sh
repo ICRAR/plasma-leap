fbb6baf64278469e383abd624ee1ba31b11ca7da
Callan Gray <cal.j.gray@gmail.com>
2021-09-16 17:59:52 +0800

    use askap for test

--- tests/integration/kubernetes/pods/minikube_start.sh
+++ tests/integration/kubernetes/pods/minikube_start.sh
@@ -3,6 +3,11 @@ eval $(minikube docker-env)
 minikube start --driver=docker --mount=true --mount-string /home/callan/Code/icrar/msdata/:/mnt/msdata/
 eval $(minikube docker-env)
 # new minikube containers requires cached docker images to be rebuilt..
-# docker build -t plasma-leap-receiver ../../../../sdp-dal-receive-leap
+## docker build -t plasma-leap-receiver ../../../../sdp-dal-receive-leap
+docker build -t ska-sdp-receive-leap ../../../../ska-sdp-receive-leap
+
+# alternative to mounting, include scripts to download msdata
+docker build -t ska-sdp-receive-leap-ci ../containers/ska-sdp-receive-leap-ci
+
 # mounts additionally require --driver=docker, --mount and --mount-string args
-minikube mount /home/callan/Code/icrar/msdata/:/msdata/
+#minikube mount /home/callan/Code/icrar/msdata/:/msdata/
--- tests/integration/kubernetes/pods/run.sh
+++ tests/integration/kubernetes/pods/run.sh
@@ -1,5 +1,1 @@
-#minikube stop
-#eval $(minikube -p minikube docker-env)
-#systemctl restart docker
-#minikube start
 kubectl apply -f ./plasma-receive-workflow-pod.yaml
